502042faf74cfaa5dcbeb42e6b3e292abf8e1ef5
GallegoDev <kauegallegosouza@gmail.com>
2024-02-28 23:06:29 -0300

    first commit

---
+++ js/tipos-dados.js
@@ -0,0 +1,32 @@
+// String
+let saudacao = 'Olá Mundo';
+console.log(saudacao);
+
+// Number
+let ano = 2024;
+console.log(ano);
+
+// Boolean
+let verdadeiro = true;
+console.log(verdadeiro);
+
+// Null
+let nulo = null;
+console.log(nulo);
+
+// Undefined
+let indefinido;
+console.log(indefinido);
+
+// Objeto
+let pessoa = {
+    nome: 'Kauê Gallego',
+    idade: 20,
+    cidade: 'São Paulo'
+}
+console.log(pessoa);
+
+// Array
+let numeros = [1, 2, 3, 4];
+console.log(numeros);
+console.log(numeros[2]);
---
+++ js/variaveis.js
@@ -0,0 +1,19 @@
+
+
+// alert('Olá Mundo!')
+console.log('Olá Mundo!');
+
+
+// Declaração usando Var
+var nome = 'Kauê Gallego';
+console.log('Meu nome é: ' + nome);
+
+
+// Declaração usando Let
+let idade = 20;
+console.log('Eu nasci em: ' + (2024 - idade));
+
+
+// Declaração usando Const
+const PI = 3.1415;
+console.log('O valor de PI é: ' + PI)
