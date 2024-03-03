b6566718c43a973af4de47c7fad86eb0a9183517
GallegoDev <kauegallegosouza@gmail.com>
2024-03-02 22:25:24 -0300

    Commit aula 01/03/2024

---
+++ 02 - Java Script Web/js/script.js
@@ -0,0 +1,10 @@
+
+//! Criação de Elementos com JavaScript
+
+const novoElemento = document.createElement('div');
+novoElemento.textContent = 'Este é um Novo Elemento';
+document.body.appendChild(novoElemento);
+
+const novoParagrafo = document.createElement('h1');
+novoParagrafo.textContent = 'Este é um Novo Paragrafo';
+document.body.appendChild(novoParagrafo);
